5347738377b7160278de3e4c563ecdc1f268f1fb
Richard Schmidt <richardnew12511@gmail.com>
2022-06-07 16:26:04 -0500

    mutation working

--- src/user/dtos/create-user.input.ts
+++ src/user/dtos/create-user.input.ts
@@ -2,8 +2,8 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class UserCreateDto {
-  @Field()
-  id: string;
+  // @Field()
+  // id: string;
 
   @Field()
   email: string;
